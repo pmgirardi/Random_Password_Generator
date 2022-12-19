798eb731c53ec2e0769c66acdf4d09fea37c58ba
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2022-12-18 22:22:32 -0800

    identified that a value needs to be returned and structuring code to meet README requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define generatePassword
+// Identified that a value needs to be returned 
+// Pseudo coding to construct code that meets ReadMe requirements
+
+function generatePassword() {
+
+  // 1. Prompt the user for the password criteria:
+  // - Password length needs to be at least 8 characters and no more than 128 characters.
+  // 2. 
+
+
+
+  return "password will go here";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
