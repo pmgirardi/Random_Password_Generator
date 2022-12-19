a3857964ee7967774ed06b352c11addd42b10e3f
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2022-12-18 22:34:21 -0800

    added array to define elements for characters

--- Develop/script.js
+++ Develop/script.js
@@ -9,11 +9,18 @@ function generatePassword() {
 
   // 1. Prompt the user for the password criteria:
   // - Password length needs to be at least 8 characters and no more than 128 characters.
-  // 2. 
+  // - Needs to include lower and upper case and special characters.
+  // 2. Ensure the input is valid.
+  // 3. Display generatePassword on the page.
+
+  // Used an array to define elements in variables of characters 
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 
 
-
-  return "password will go here";
 }
 
 // Write password to the #password input
